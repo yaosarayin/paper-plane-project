bc61b0077ca4ece68ff5af0d48a62038eec02f97
Yao <yyin@college.harvard.edu>
2020-10-25 11:37:08 -0400

    Clean up

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,10 +5,11 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---src-pages-about-js": hot(preferDefault(require("/Users/ela/Yao/Dev/paper-plane-project/src/pages/about.js"))),
-  "component---src-pages-blog-js": hot(preferDefault(require("/Users/ela/Yao/Dev/paper-plane-project/src/pages/blog.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/ela/Yao/Dev/paper-plane-project/src/pages/index.js"))),
-  "component---src-pages-join-js": hot(preferDefault(require("/Users/ela/Yao/Dev/paper-plane-project/src/pages/join.js"))),
-  "component---src-templates-blog-posts-js": hot(preferDefault(require("/Users/ela/Yao/Dev/paper-plane-project/src/templates/BlogPosts.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/yao/Dev/paper-plane-project/.cache/dev-404-page.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("/Users/yao/Dev/paper-plane-project/src/pages/about.js"))),
+  "component---src-pages-blog-js": hot(preferDefault(require("/Users/yao/Dev/paper-plane-project/src/pages/blog.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/yao/Dev/paper-plane-project/src/pages/index.js"))),
+  "component---src-pages-join-js": hot(preferDefault(require("/Users/yao/Dev/paper-plane-project/src/pages/join.js"))),
+  "component---src-templates-blog-posts-js": hot(preferDefault(require("/Users/yao/Dev/paper-plane-project/src/templates/BlogPosts.js")))
 }
 
