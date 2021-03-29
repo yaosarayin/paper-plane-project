7de8c93b09e7014aa82e8f049fd880abb582facb
Yao (sara?) Yin <yyin@college.harvard.edu>
2021-03-29 10:50:51 -0400

    Fix buildtime error

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,10 +4,8 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\yay474\\Dev\\paper-plane-project\\.cache\\dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("C:\\Users\\yay474\\Dev\\paper-plane-project\\src\\pages\\404.js")),
   "component---src-pages-about-js": preferDefault(require("C:\\Users\\yay474\\Dev\\paper-plane-project\\src\\pages\\about.js")),
-  "component---src-pages-blog-js": preferDefault(require("C:\\Users\\yay474\\Dev\\paper-plane-project\\src\\pages\\blog.js")),
   "component---src-pages-join-js": preferDefault(require("C:\\Users\\yay474\\Dev\\paper-plane-project\\src\\pages\\join.js")),
   "component---src-templates-blog-post-js": preferDefault(require("C:\\Users\\yay474\\Dev\\paper-plane-project\\src\\templates\\blog-post.js")),
   "component---src-templates-home-page-js": preferDefault(require("C:\\Users\\yay474\\Dev\\paper-plane-project\\src\\templates\\home-page.js"))
